a22fb5cee18bb05a0bc2c5f94abe30397a29c501
Christoph Kruger <christoph_kruger@daytonastate.edu>
2020-03-10 22:47:23 -0400

    added models for GB,Age,& Boost

---
+++ app/Http/Controllers/BoostController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Boost;
+use Illuminate\Http\Request;
+
+class BoostController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Boost  $boost
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Boost $boost)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Boost  $boost
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Boost $boost)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Boost  $boost
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Boost $boost)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Boost  $boost
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Boost $boost)
+    {
+        //
+    }
+}
---
+++ database/factories/GreatBuildingFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\GreatBuilding;
+use Faker\Generator as Faker;
+
+$factory->define(GreatBuilding::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
