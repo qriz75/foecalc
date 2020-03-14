e93526e1a794b00727d8dcc1ab2228d272e2988f
qriz75 <christoph_kruger@daytonastate.edu>
2020-03-14 17:27:52 -0400

    more 404 fixes

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -5,9 +5,10 @@
     
 
     <div class="content">
-        <div class="text-responsive"><h3>"I don't know ...<br> It was supposed to be here !!!"</h3>
+        <div class="text-responsive"><h2>"I don't know ...</h2><br><h4> It was supposed to be here !!!"</h4>
+            <div class="wrapper404"><img src="img/404.gif" alt="foecalc"></div>  
         <div class="title m-b-md">
-            <img src="img/404.gif" alt="foecalc">            
+                      
         </div>
     </div>
         
